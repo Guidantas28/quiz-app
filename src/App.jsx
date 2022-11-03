385021c9188bbca1a0fe105c0a98a508c63948e6
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-11-03 15:26:52 -0300

    started the quiz and add the function to reorder the questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useContext, useEffect } from 'react'
 import './App.css'
+import { Question } from './components/Questions'
 import { Welcome } from './components/Welcome'
 
+import { QuizContext } from './context/Quiz'
+
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    dispatch({ type: 'REORDER_QUESTIONS' })
+  }, [])
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
     </div>
   )
 }
---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import Quiz from '../../assets/quiz.svg'
+import { QuizContext } from '../../context/Quiz'
+
+import './styles.css'
+
+export const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: 'CHANGE_STATE' })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do quiz" />
+    </div>
+  )
+}
