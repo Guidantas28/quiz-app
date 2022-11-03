f5eca040826ea8d6127a2412800e044468dae2d5
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-11-03 18:31:59 -0300

    finished the application and finished the css

--- src/components/GameOver/index.jsx
+++ src/components/GameOver/index.jsx
@@ -11,8 +11,8 @@ const GameOver = () => {
       <h2>Fim de jogo!</h2>
       <p>Pontuação: {quizState.score}</p>
       <p>
-        Você acertou {quizState.score} de {quizState.questions.length}
-        perguntas.
+        Você acertou {quizState.score} de {quizState.questions.length}{' '}
+         perguntas.
       </p>
       <img src={WellDone} alt="Fim do Quiz" />
       <button onClick={() => dispatch({ type: 'NEW_GAME' })}>Reiniciar</button>
--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -1,24 +1,43 @@
 import { useContext } from 'react'
 import { QuizContext } from '../../context/Quiz'
+import Option from '../Options'
 import './styles.css'
 
 const Question = () => {
   const [quizState, dispatch] = useContext(QuizContext)
   const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+  const onSelectOption = (option) => {
+    dispatch({
+      type: 'CHECK_ANSWER',
+      payload: { answer: currentQuestion.answer, option },
+    })
+  }
+
   return (
     <div id="questions">
-      Question
+      <strong>Question</strong>
       <p>
         Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}
       </p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
-        <p>Opções</p>
+        <p>
+          {currentQuestion.options.map((option) => (
+            <Option
+              key={option}
+              option={option}
+              answer={currentQuestion.answer}
+              selectOption={() => onSelectOption(option)}
+            />
+          ))}
+        </p>
       </div>
-      <button onClick={() => dispatch({ type: 'CHANGE_QUESTION' })}>
-        Continuar
-      </button>
+      {quizState.answerSelected && (
+        <button onClick={() => dispatch({ type: 'CHANGE_QUESTION' })}>
+          Continuar
+        </button>
+      )}
     </div>
   )
 }
